dc486a4e80ea27bc27d848e262a075e0991bf907
root <daniel@danielpalstra.com>
2017-06-07 20:36:29 +0000

    1st working version with installation of ansible and docker by ansible playbooks

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Setup a dev-host
 
-Run some ansible playbooks to set up a development host on any vm.
+Run some ansible playbooks to set up a development host on any vm. The script assumes it's running
+under root which should be illegal.
 
-'curl -sSL https://get.docker.com/ | sh'
+```shell
+curl -sSL https://raw.githubusercontent.com/danielpalstra/dev-host/master/install.sh | bash
+```
+
+Test the install script in an Ubuntu 16.04 Docker container.
+
+```shell
+docker run --privileged -it -v ${PWD}:/config dev-host bash -c "cd /config && sh install.sh && bash"
+```
+
+## TODO
+
+- Install user config from git repository
+- Install development tooling
+- Create non-root user
+
--- install.sh
+++ install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+REPO_URL=https://github.com/danielpalstra/dev-host.git
+
 set -x 
 
 echo "Starting installation of all kind of crap to get a development host"
+
+echo "Installing dependencies"
+sudo apt update -y && \
+sudo apt install -y git
+
+# move to tmp to install from
+cd /tmp
+
+# First clone te complete git repo
+git clone $REPO_URL /tmp/dev-host && \
+cd /tmp/dev-host
+
+ls -l 
+
+exit 1
+
 # install ansible
-
 echo "Installing latest ansible"
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -y
+sudo apt-get install -y ansible
 
-# install required playbook
-ansible-galaxy install mongrelion.docker
+# install required playbooks
+ansible-galaxy install -r requirements.yml
 
+echo "Running ansible playbook"
 # run playbook
 ansible-playbook -i hosts dev-host.yml
+
+echo "Finished..."
